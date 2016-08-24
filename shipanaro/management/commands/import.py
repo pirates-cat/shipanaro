ae33a62aee1619d56243a6c8742849765e145ee8
Dario Castañé <i@dario.im>
2016-08-24 14:34:20 +0200

    Improving import process

--- shipanaro/management/commands/import.py
+++ shipanaro/management/commands/import.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import transaction
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import (
@@ -9,6 +10,7 @@ from hashers_passlib.converters import ldap_sha1, ldap_md5
 from shipanaro.hashers import ldap_salted_sha1
 from shipanaro.models import Membership, Subscription
 import pickle
+import pytz
 
 
 class Porygon:
@@ -65,6 +67,7 @@ class PasswordConverter:
 
 class Command(BaseCommand):
     help = 'Imports pickled users map from file'
+    pwc = PasswordConverter()
 
     def add_arguments(self, parser):
         parser.add_argument('file', nargs=1)
@@ -72,46 +75,68 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         filename = options['file'][0]
         f = open(filename, 'rb')
-        pwc = PasswordConverter()
         # Latin1 encoding used because this command was developed to import
         # from an old system with Python 2.5 (no typo, 2.5).
         # Although strings were exported as Unicode (you know, u'blahblah'),
         # I guess we are dealing with THE PAST.
         users = pickle.load(f, encoding='latin1')
-        src = None
         try:
+            src = None
             with transaction.atomic():
                 for email in users:
                     src = users[email].copy()
-                    m = Porygon(Membership())
-                    remaining = m.morph(users[email])
-                    u = Porygon(User())
-                    remaining = u.morph(remaining)
-                    u.payload.password = pwc.convert(u.payload.password)
-                    s = None
-                    if 'newsletter' in remaining:
-                        s = Subscription()
-                        s.service = 'newsletter'
-                        if isinstance(u.payload.email, list):
-                            s.endpoint = u.payload.email[1]
-                        else:
-                            s.endpoint = u.payload.email
-                        del remaining['newsletter']
+                    m, remaining = self.__load_membership(users[email])
+                    u, remaining = self.__load_user(remaining)
+                    s, remaining = self.__load_subscription(u, remaining)
                     if len(remaining) > 0:
-                        raise Exception("Unassigned attributes: %s" % ', '.join(
-                            remaining.keys()))
-                    if isinstance(u.payload.email, list):
-                        if len(u.payload.email) > 2:
-                            raise Exception("Too much e-mails!")
-                        u.payload.email = u.payload.email[0]
-                    u.payload.save()
-                    m.payload.user = u.payload
-                    m.payload.save()
-                    if s:
-                        s.member = m.payload
-                        s.save()
+                        raise Exception("Unassigned attributes: %s" % ', '.
+                                        join(remaining.keys()))
+                    self.__save(u, m, s)
         except Exception as e:
             if src:
                 import pprint
                 pprint.pprint(src)
                 raise e
+
+    def __load_membership(self, data):
+        m = Porygon(Membership())
+        data = m.morph(data)
+        mp = m.payload
+        if mp.date_left:
+            mp.date_left = pytz.utc.localize(datetime.strptime(
+                mp.date_left, '%Y-%m-%d'))
+        mp.birthday = pytz.utc.localize(datetime.strptime(
+            mp.birthday, '%Y-%m-%d'))
+        return (mp, data)
+
+    def __load_user(self, data):
+        u = Porygon(User())
+        data = u.morph(data)
+        up = u.payload
+        up.password = self.pwc.convert(up.password)
+        up.date_joined = pytz.utc.localize(datetime.strptime(
+            up.date_joined, '%Y-%m-%d'))
+        if up.last_login:
+            up.last_login = pytz.utc.localize(datetime.strptime(
+                up.last_login, '%Y-%m-%d'))
+        return (up, data)
+
+    def __load_subscription(self, user, data):
+        s = None
+        if 'newsletter' in data:
+            s = Subscription()
+            s.service = 'newsletter'
+            if isinstance(user.email, list):
+                s.endpoint = user.email[1]
+            else:
+                s.endpoint = user.email
+            del data['newsletter']
+        return (s, data)
+
+    def __save(self, user, membership, subscription):
+        user.save()
+        membership.user = user
+        membership.save()
+        if subscription:
+            subscription.member = membership
+            subscription.save()
