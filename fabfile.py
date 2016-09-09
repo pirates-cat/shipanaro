1d3a5e2effbd3d3e6b7ad43336b97475495472e6
Dario Castañé <i@dario.im>
2016-09-09 21:23:04 +0200

    YAPF fab task

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,18 @@ env.local_static_root = './static'
 
 env.remote_static_root = env.www_home + '/%s'
 
+
+def setup_static():
+    run('adduser www --home %s' % env.www.home)
+    run('passwd -d www')
+    run('passwd -l www')
+    run('chown -R www: %s' % env.www.home)
+    run('mkdir -p %s' % env.www.home)
+    run('chmod 700 %s/.ssh' % env.www.home)
+    run('touch %s/.ssh/authorized_keys' % env.www.home)
+    run('chmod 400 %s/.ssh/authorized_keys' % env.www.home)
+
+
 def deploy_static(host):
     local('python manage.py collectstatic --no-input --verbosity 0')
     run('mkdir -p ' + env.remote_static_root % host)
@@ -15,15 +27,8 @@ def deploy_static(host):
         local_dir=env.local_static_root,
         delete=True,
         ssh_opts='-q',
-        extra_opts='--quiet --no-motd',
-    )
+        extra_opts='--quiet --no-motd', )
 
-def setup_static():
-    run('adduser www --home %s' % (env.www.home))
-    run('passwd -d www')
-    run('passwd -l www')
-    run('chown -R www: %s' % (env.www.home))
-    run('mkdir -p %s' % (env.www.home))
-    run('chmod 700 %s/.ssh' % (env.www.home))
-    run('touch %s/.ssh/authorized_keys' % (env.www.home))
-    run('chmod 400 %s/.ssh/authorized_keys' % (env.www.home))
+
+def format(extra_dirs=''):
+    local('yapf -i -r --style pep8 shipanaro/ %s' % extra_dirs)
