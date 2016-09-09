3df306f16e7d0829532fde221adb7c4617bfe967
Dario Castañé <i@dario.im>
2016-09-09 21:16:57 +0200

    First deployment adjustments

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+from fabric.api import *
+from fabric.contrib import project
+
+env.www_home = '/var/www'
+
+env.local_static_root = './static'
+
+env.remote_static_root = env.www_home + '/%s'
+
+def deploy_static(host):
+    local('python manage.py collectstatic --no-input --verbosity 0')
+    run('mkdir -p ' + env.remote_static_root % host)
+    project.rsync_project(
+        remote_dir=env.remote_static_root % host,
+        local_dir=env.local_static_root,
+        delete=True,
+        ssh_opts='-q',
+        extra_opts='--quiet --no-motd',
+    )
+
+def setup_static():
+    run('adduser www --home %s' % (env.www.home))
+    run('passwd -d www')
+    run('passwd -l www')
+    run('chown -R www: %s' % (env.www.home))
+    run('mkdir -p %s' % (env.www.home))
+    run('chmod 700 %s/.ssh' % (env.www.home))
+    run('touch %s/.ssh/authorized_keys' % (env.www.home))
+    run('chmod 400 %s/.ssh/authorized_keys' % (env.www.home))
--- shipanaro/settings.py
+++ shipanaro/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Django REST Framework
