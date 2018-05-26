5a1355d1f6a8c60d49b8a0237485cd04a78ef703
Dario Castañé <i@dario.im>
2018-05-26 11:15:54 +0200

    GDPR basic support

--- shipanaro/gdpr/models.py
+++ shipanaro/gdpr/models.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Consent(models.Model):
-    pass
-    # signal
+    user = models.ForeignKey(User)
+    purpose = models.CharField(max_length=64)
+    creation_time = models.DateTimeField(auto_now_add=True)
